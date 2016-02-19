9616f000f8db1d94bc61097268e562ce3a2dd8cb
duncan-napier <duncan.napier@TSC-01011.ts-catapult.org.uk>
2016-02-19 16:11:44 +0000

    Test
    Updating comments

--- airport_kml.py
+++ airport_kml.py
@@ -2,11 +2,14 @@
 #	airport resource reader
 #
 
+
 import os, sys
 
+# how to use this script
 def usage():
 	print 'Usage\n~~~~~\nairport_kml.py <airport csv file>\n'
 	
+# main function
 def main():
 	try:
 		csv_file = sys.argv[1]
@@ -17,5 +20,6 @@ def main():
 	output_path = os.path.basename( csv_file ) + '.kml'
 	print 'Writting to file.... ' + output_path
 	
+# entry point to the script
 if __name__ == '__main__':
 	main()
